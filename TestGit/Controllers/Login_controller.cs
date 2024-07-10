9266a484482fb10c31c676461b4b6bbe0ca47051
Astro-Mars <lwinkok@gmail.com>
2024-07-11 01:27:31 +0900

    Add Login_controller

---
+++ TestGit/Controllers/Login_controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestGit.Controllers
+{
+    public class Login_controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
